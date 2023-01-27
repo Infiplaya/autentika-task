5568c40d467a68bce0ba9484fb563922f8177130
Pawel Sobaniec <infipablosoban@gmail.com>
2023-01-27 13:05:56 +0100

    feat: display list of characters

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,41 @@
 import Head from "next/head";
 import { Inter } from "@next/font/google";
+import { gql } from "@apollo/client";
+import client from "../apollo-client";
+import Image from "next/image";
+
+export async function getStaticProps() {
+  const { data } = await client.query({
+    query: gql`
+      query Characters {
+        characters {
+          results {
+            id
+            name
+            image
+          }
+        }
+      }
+    `,
+  });
+
+  return {
+    props: {
+      characters: data.characters.results,
+    },
+  };
+}
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+interface Character {
+  id: number;
+  name: string;
+  image: string;
+}
+
+export default function Home({ characters }: { characters: Character[] }) {
+  console.log(characters);
   return (
     <>
       <Head>
@@ -12,7 +44,21 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>Hello, world</main>
+      <main>
+        <div>
+          {characters.map((character) => (
+            <div key={character.id}>
+              <Image
+                src={character.image}
+                alt="Character"
+                width={250}
+                height={250}
+              />
+              <p>{character.name}</p>
+            </div>
+          ))}
+        </div>
+      </main>
     </>
   );
 }
