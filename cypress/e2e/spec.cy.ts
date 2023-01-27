e7a35e6e7d50bf8a87e0588ead8d8b7c532c0175
Pawel Sobaniec <infipablosoban@gmail.com>
2023-01-27 20:12:25 +0100

    feat: add cypress and simple tests

---
+++ cypress/e2e/spec.cy.ts
@@ -0,0 +1,12 @@
+describe('Navigation', () => {
+  it('should navigate to the Albert Einstein page', () => {
+    // Start from the index page
+    cy.visit('http://localhost:3000/')
+
+    cy.get('a[href*="character/11"]').click()
+
+    // The new page title should include Albert Einstein
+    cy.title().should("equal", "Albert Einstein")
+    cy.url().should("include", "/character/11")
+  })
+})
